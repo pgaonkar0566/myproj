952cfa8861a81e9e29af86aca1b7b384aecdcdea
pallavi <globaluser@gmail.com>
2025-08-13 05:14:32 +0000

    Initial commit: Hello World Maven project

---
+++ src/main/java/com/pallavi/App.java
@@ -0,0 +1,10 @@
+package com.pallavi;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello DevOps!");
+    }
+}
---
+++ src/test/java/com/pallavi/AppTest.java
@@ -0,0 +1,26 @@
+package com.pallavi;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() { 
+       assertTrue(true);
+}
+@Test
+    public void shouldAnswerWithTrue1() {
+       assertTrue(true);
+
+
+
+    }
+}
